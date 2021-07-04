eb594a0ed29edc3bf27e1ae6bf5292955d3177c7
benjaminli7 <nicky01.bl@gmail.com>
2021-07-04 15:25:31 +0200

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", function(event) {
+    document.querySelector('body').classList.remove('preload');
+});
+
+const wrapper = document.getElementById("wrapper");
+const hamburger = document.getElementById("hamburger");
+const nav_menu = document.getElementById("nav-menu");
+const close_menu = document.getElementById("close-menu");
+
+hamburger.onclick = () => {
+    hamburger.style.display = "none";
+    close_menu.style.display = "block";
+    nav_menu.style.display = "block";
+    wrapper.classList.add("overlay");
+}
+
+close_menu.onclick = () => {
+    hamburger.style.display = "block";
+    close_menu.style.display = "none";
+    nav_menu.style.display = "none";
+    wrapper.classList.remove("overlay");
+}
+
+
+
